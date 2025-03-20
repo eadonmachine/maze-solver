5ad8a14ea3b638053e6dfa116139f8f7e323b199
eadonmachine <eadonmachine@gmail.com>
2025-03-20 14:29:39 +0000

    add draw lines between cells

--- cell.py
+++ cell.py
@@ -29,3 +29,15 @@ class Cell:
         if self.has_left_wall:
             left_wall = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
             self._win.draw_line(left_wall)
+
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            color = "gray"
+        else:
+            color = "red"
+
+        line = Line(
+            Point((self._x1 + self._x2)/2, (self._y1 + self._y2)/2),
+            Point((to_cell._x1 + to_cell._x2)/2, (to_cell._y1 + to_cell._y2)/2)
+        )
+        self._win.draw_line(line, color)
