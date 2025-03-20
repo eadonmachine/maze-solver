420cbfdbaa28c33d0bcee8955c3ea73f8c0ac0e7
eadonmachine <eadonmachine@gmail.com>
2025-03-20 19:58:20 +0000

    break entrance and exit of maze

--- cell.py
+++ cell.py
@@ -19,18 +19,30 @@ class Cell:
         self._y1 = y1
         self._x2 = x2
         self._y2 = y2
+
+        line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
         if self.has_top_wall:
-            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
             self._win.draw_line(line)
+        else:
+            self._win.draw_line(line, "white")
+
+        line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
         if self.has_right_wall:
-            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
             self._win.draw_line(line)
+        else:
+            self._win.draw_line(line, "white")
+
+        line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
         if self.has_bottom_wall:
-            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self._win.draw_line(line)
+        else:
+            self._win.draw_line(line, "white")
+
+        line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
         if self.has_left_wall:
-            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
             self._win.draw_line(line)
+        else:
+            self._win.draw_line(line, "white")
 
     def draw_move(self, to_cell, undo=False):
         if self._win is None:
