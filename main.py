ebee48ccd1400a6617ddaac157e9aa96d2c6764c
eadonmachine <eadonmachine@gmail.com>
2025-03-20 03:05:46 +0000

    draw lines

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(250,150), Point(60, 300))
+    line2 = Line(Point(30,400), Point(300,450))
+    win.draw_line(line, "red")
+    win.draw_line(line2)
     win.wait_for_close()
 
 main()
